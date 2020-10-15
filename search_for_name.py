4abd95356c1389f928c17ca24901cd50e4975168
brend3n <brenden@knights.ucf.edu>
2020-10-15 14:32:07 -0400

    Modified some of the code to show which part of the name is found

--- search_for_name.py
+++ search_for_name.py
@@ -5,12 +5,14 @@ desired_last_name = sys.argv[2].capitalize()
 
 
 # print(desired_first_name + " " +desired_last_name)
+flag_f = flag_l = False
 
 ff = open("first_name.txt", "r")
 
 for line in ff:
 	if line.strip() == (desired_first_name):
-		print("Found: " + desired_first_name)
+		# print("Found: " + desired_first_name)
+		flag_f = True
 		break
 ff.close()
 
@@ -19,6 +21,16 @@ ff.close()
 fl = open("last_name.txt", "r")
 for line in fl:
 	if line.strip() == (desired_last_name):
-		print("Found: " + desired_last_name)
+		# print("Found: " + desired_last_name)
+		flag_l = True
 		break
 fl.close()
+
+if flag_f and flag_l:
+	print("Found: " + desired_first_name + " " + desired_last_name)
+elif flag_f:
+	print("Only Found: " + desired_first_name)
+elif flag_l:
+	print("Only Found: " + desired_last_name)
+else:
+	print("Not Found")
