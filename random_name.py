b972233a5340dc0210514125821f9881c3d9406c
brend3n <brenden@knights.ucf.edu>
2021-12-09 11:38:02 -0500

    Made modulized code

--- random_name.py
+++ random_name.py
@@ -22,17 +22,6 @@ def randomName():
 
 
 
-#	f = open("last_name_old.txt", "r")
-#	f2 = open("last_name.txt", "wt")
-
-#	for line in f:
-#		s = line.lower()
-#		s = s.capitalize()
-#		f2.write(s)
-
-#	f.close()
-#	f2.close()
-
 	f = open("last_name.txt", "r")
 	i = 1
 
@@ -47,11 +36,53 @@ def randomName():
 
 	last_name = last_name.replace('\n', '')
 
-	print(first_name  + " " +last_name)
+	name = f"{first_name} {last_name}"
+	print(name)
+	return name
+
+def random_first_name():
+	ran_first = random.randint(1, 18239)
+
+	f = open("./Random-Name-Generator/first_name.txt", "r")
+	i = 1;
+
+	for x in f:
+	    if i == ran_first :
+	        first_name = x
+	        break
+
+	    i = i + 1
+
+	f.close()
+	first_name = first_name.replace('\n', '')
+	print(first_name)
+	return first_name
+
+def random_last_name():
+	f = open("./Random-Name-Generator/last_name.txt", "r")
+	i = 1
+	ran_last = random.randint(1, 78475)
+
+
+	for y in f:
+	    if i == ran_last:
+	        last_name = y
+	        break
+	    i = i + 1
+
+	f.close()
+
+	last_name = last_name.replace('\n', '')
+	print(last_name)
+	return last_name
+
+
+
+# random_first_name()
+# random_last_name()
+# name = randomName()
 
 
 
 
-for i in range(10000):
-	time.sleep(.5)
-	randomName()
+
