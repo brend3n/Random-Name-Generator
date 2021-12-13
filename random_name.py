f3b5f0a997c38554c8ef15a3ef9be1707e9031db
brend3n <brenden@knights.ucf.edu>
2021-12-12 19:37:00 -0500

    Optimized random name generation

--- random_name.py
+++ random_name.py
@@ -1,9 +1,9 @@
 import random
 import time
 
+# ! Outdated
 def randomName():
 	ran_first = random.randint(1, 18239)
-
 	ran_last = random.randint(1, 78475)
 
 	# print(f"ran_first: {ran_first} rand_last: {ran_last}")
@@ -12,7 +12,7 @@ def randomName():
 	i = 1;
 
 	for x in f:
-	    if i == ran_first :
+	    if i == ran_first:
 	        first_name = x
 	        break
 
@@ -40,6 +40,7 @@ def randomName():
 	print(name)
 	return name
 
+# ! Outdated
 def random_first_name():
 	ran_first = random.randint(1, 18239)
 
@@ -58,6 +59,7 @@ def random_first_name():
 	print(first_name)
 	return first_name
 
+# ! Outdated
 def random_last_name():
 	f = open("./Random-Name-Generator/last_name.txt", "r")
 	i = 1
@@ -77,10 +79,55 @@ def random_last_name():
 	return last_name
 
 
+# Takes a path to the root directory
+def random_name_updated(path=""):
+    last_names = []
+    first_names = []
+    
+    first_name_file_path = f"{path}first_name.txt"
+    last_name_file_path = f"{path}last_name.txt"
+    
+    with (open(first_name_file_path, "r")) as f:
+        first_names = f.readlines()
+    f.close()
+    
+    with (open(last_name_file_path, "r")) as f:
+        last_names = f.readlines()
+    f.close()
+    
+    first_name = random.choice(first_names).replace("\n", "") 
+    last_name = random.choice(last_names).replace("\n", "")
+    
+    name = f"{first_name} {last_name}"
+    return name
+	
+
+def random_first_name_updated(path):
+    first_names = []
+    first_name_file_path = f"{path}first_name.txt"
+    with (open(first_name_file_path, "r")) as f:
+        first_names = f.readlines()
+    f.close()
+    first_name = random.choice(first_names).replace("\n", "") 
+    return first_name
+
+def random_last_name_updated(path=""):
+    last_names = []
+    last_name_file_path = f"{path}last_name.txt"
+
+    with (open(last_name_file_path, "r")) as f:
+        last_names = f.readlines()
+    f.close()
+    last_name = random.choice(last_names).replace("\n", "")
+    return last_name
+
+
 
 # random_first_name()
 # random_last_name()
 # name = randomName()
+# print("test")
+# print(random_name_updated())
 
 
 
